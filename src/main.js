a397babc3dbf9dc83c1ff0ad7be0a713e2aa1966
weiweiweiweiweiwiewiwerwerdggw <xianyao.wei.de@gmail.com>
2025-10-24 16:03:58 +0200

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import './style.scss'
+
+const toggleButton = document.getElementById('theme-toggle');
+const root = document.documentElement;
+
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme) {
+  root.setAttribute('data-theme', savedTheme);
+}
+
+toggleButton.addEventListener('click', () => {
+  const currentTheme = root.getAttribute('data-theme');
+  if (currentTheme === 'dark') {
+    root.removeAttribute('data-theme'); // defaults to light
+    localStorage.setItem('theme', 'light');
+  } else {
+    root.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+  }
+});
